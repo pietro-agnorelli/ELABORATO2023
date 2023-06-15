5db30f880d5e113e4400f90231f1b3e7a42c8674
Pietro <pietro.agnorelli@stud.unifi.it>
2023-06-15 18:36:01 +0200

    Added getter and setter method to Product

--- Product.h
+++ Product.h
@@ -10,6 +10,28 @@
 class Product{
 public:
     explicit Product(std::string name, std::string type="null", int q=0): name(name), type(type), quantity(q) {};
+
+    const std::string &getName() const {
+        return name;
+    }
+    const std::string &getType() const {
+        return type;
+    }
+    int getQuantity() const {
+        return quantity;
+    }
+
+    void setName(const std::string &name) {
+        Product::name = name;
+    }
+    void setType(const std::string &type) {
+        Product::type = type;
+    }
+    void setQuantity(int quantity) {
+        Product::quantity = quantity;
+    }
+
+private:
     std::string name;
     std::string type;
     int quantity;
